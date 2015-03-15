47a4a5acc63e2d883077b3cb8a2328bed8fa4261
xhagrg <grg.iksha@hotmail.com>
2015-03-15 20:49:52 +0545

    Add routes.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   root 'home#index'
+  resources :users, only: [:index, :show] do
+    get :playlist
+  end
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
