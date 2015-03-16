ff297b4f3fa85c0f9b706c876d8cdeed841cdc49
xhagrg <grg.iksha@hotmail.com>
2015-03-16 10:15:11 +0545

    Add method to convert id to string.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,12 @@ class User
   field :first_name, type: String
   field :last_name, type: String
   field :middle_name, type: String
+  field :nick_name, type: String
   field :admin, type: Boolean
+
+  def id
+    _id.to_s
+  end
 
   def full_name
     [first_name, middle_name, last_name].compact.map(&:humanize).join(' ')
