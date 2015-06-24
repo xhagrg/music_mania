b0f746b8cc5107f7f139b5ed343c06c4064bd76c
xhagrg <grg.iksha@hotmail.com>
2015-06-24 23:40:38 +0545

    Add show action for user.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,10 @@ class UsersController < ApplicationController
                User.all
              end
   end
+
+  def show
+    @user = User.where(_id: params[:id]).first
+    playlist = @user.playlists.first || @user.playlists.create
+    @songs = playlist.songs
+  end
 end
