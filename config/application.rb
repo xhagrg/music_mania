c028970a2366c130f6c1d2f0e1c3fb1d69256a1e
Milan Thapa <milans.thapa78@gmail.com>
2015-07-03 00:05:15 +0545

    implement dynamic video js player

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,7 @@ module MusicMania
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.autoload_paths += Dir["#{config.root}/lib"]
     config.assets.paths << Rails.root.join("vendor","assets","bower_components")
     config.assets.paths << Rails.root.join("vendor","assets","bower_components","bootstrap-sass-official","assets","fonts")
     config.assets.precompile << %r(.*.(?:eot|svg|ttf|woff)$)
