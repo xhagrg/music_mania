9b2e14d5204ce92a68f047e024d92523e6f97cff
xhagrg <grg.iksha@hotmail.com>
2015-07-05 13:16:11 +0545

    Handle case when authorization was failing.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,5 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_filter :authenticate_user!
+  skip_before_filter :verify_authenticity_token  
 end
