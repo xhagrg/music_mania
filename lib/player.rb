9f19c73b98e5a0158fdf7a77635303beefb588f6
xhagrg <grg.iksha@hotmail.com>
2015-09-24 07:28:33 +0545

    Fix player to send respective song.

--- lib/player.rb
+++ lib/player.rb
@@ -1,14 +1,23 @@
 class Player
-  attr_accessor :user, :songs, :current_playlist, :mode
+  CURRENT_INDEX = 'current_index'
+
+  attr_accessor :user, :song, :current_playlist, :mode, :index
   def initialize(user)
     @user = user
     @current_playlist = user.get_playlist
-    @songs = current_playlist.songs
+    @song = current_playlist.songs[current_index] || Song.first
     # @mode = player.mode # modes: {NORMAL,RANDOM,SELECTIVE,SUPER-FILTERED}
   end
 
   def build_data
-    song = songs.first
-    { src: song.url, type: song.extension, index: 1 }.to_json
+    { src: song.url, type: song.extension, index: current_index }.to_json
+  end
+
+  def current_index
+    @index = $redis.hget(CURRENT_INDEX, user.id.to_s) || 0
+  end
+
+  def increment_index
+    @index = $redis.hincrby(CURRENT_INDEX, user.id.to_s, 1)
   end
 end
