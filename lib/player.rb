7740a58b31d514ed9d6fbe6511ba4a48f0284d79
xhagrg <grg.iksha@hotmail.com>
2015-09-24 18:03:28 +0545

    Change logic to fetch new song.

--- lib/player.rb
+++ lib/player.rb
@@ -5,19 +5,32 @@ class Player
   def initialize(user)
     @user = user
     @current_playlist = user.get_playlist
-    @song = current_playlist.songs[current_index] || Song.first
+    @song = current_song
     # @mode = player.mode # modes: {NORMAL,RANDOM,SELECTIVE,SUPER-FILTERED}
   end
 
   def build_data
-    { src: song.url, type: song.extension, index: current_index }.to_json
+    { src: song.url, type: song.extension }.to_json
   end
 
   def current_index
-    @index = $redis.hget(CURRENT_INDEX, user.id.to_s) || 0
+    @index = $redis.hget(CURRENT_INDEX, user.id.to_s).to_i
+  end
+
+  def current_song
+    current_playlist.songs[current_index] || Song.first
+  end
+
+  def reset_index
+    $redis.hset(CURRENT_INDEX, user.id.to_s, 0)
   end
 
   def increment_index
-    @index = $redis.hincrby(CURRENT_INDEX, user.id.to_s, 1)
+    songs_size = @current_playlist.songs.size
+    if index >= songs_size
+      reset_index
+    elsif @current_playlist.songs.size > 0
+      @index = $redis.hincrby(CURRENT_INDEX, user.id.to_s, 1)
+    end
   end
 end
